d08f5aea005245579321d99af77fa324e9ad3d86
Julian Cataldo <contact@juliancataldo.com>
2022-08-07 19:45:52 +0200

    fix: check yaml presence + extract function

--- index.ts
+++ index.ts
@@ -13,6 +13,7 @@ import addFormats from 'ajv-formats';
 import { VFileMessage } from 'vfile-message';
 import { lintRule } from 'unified-lint-rule';
 import type { Node } from 'unist';
+import type { VFile } from 'unified-lint-rule/lib';
 /* —————————————————————————————————————————————————————————————————————————— */
 // FIXME: find correct source type instead of overloading this one
 type NodeWithChildren = Node & { children?: { value: string }[] };
@@ -23,65 +24,73 @@ type Frontmatter = { $schema?: string; [key: string]: any };
 const ajv = new Ajv({ allErrors: true });
 addFormats(ajv);
 
+function validateFrontmatter(rawYaml: string, file: VFile) {
+  /* Parse the YAML string, previously extracted by `remark-frontmatter` */
+  const data: Frontmatter = yaml.parse(rawYaml);
+
+  /* Get the user-defined YAML `$schema` for current Markdown file */
+  const schemaPath = data.$schema;
+  /* Path is combined with process current working directory */
+  const schemaFullPath = path.join(process.cwd(), schemaPath);
+  const schema = yaml.parse(
+    // IDEA: make it async?
+    fs.readFileSync(schemaFullPath, 'utf-8'),
+  );
+  /* `$schema` is now extracted, remove it so it will not interfere */
+  delete data.$schema;
+
+  /* JSON Schema compilation + validation with AJV */
+  const validate = ajv.compile(schema);
+  validate(data);
+
+  /* Push JSON Schema validation failures messages */
+  validate?.errors?.forEach((error) => {
+    /* Capitalize error message */
+    const errMessage =
+      `${error.message.charAt(0).toUpperCase()}` +
+      `${error.message.substring(1)}`;
+    /* Parent or sub-property error */
+    const reason = error.instancePath
+      ? `${error.instancePath}: ${errMessage}`
+      : errMessage;
+
+    const message = new VFileMessage(
+      reason,
+      // TODO: find a way to map code range to validation errors
+      // position,
+      null,
+      // NOTE: `origin` doesn't seems to be leveraged by anything
+      // 'origin',
+    );
+
+    /* Assemble and format pretty per-error insights for end-user */
+    message.note = yaml
+      .stringify({
+        keyword: error.keyword,
+        params: error.params,
+        $schema: `${schemaPath}/${error.schemaPath}`,
+      })
+      .trim();
+    // FIXME: beware that this is not working correctly with `auto-fix`,
+    // can be dangerous (wrong code range)!
+    message.expected = error?.params?.allowedValues;
+
+    file.messages.push(message);
+  });
+}
+
 const remarkFrontmatterSchema = lintRule(
   {
     origin: 'remark-lint:frontmatter-schema',
     url: 'https://github.com/JulianCataldo/remark-lint-frontmatter-schema',
   },
   (ast: NodeWithChildren, file) => {
-    /* Parse the YAML string, previously extracted by `remark-frontmatter` */
-    const rawYaml = ast?.children[0].value;
-    const data: Frontmatter = yaml.parse(rawYaml);
-
-    /* Get the user-defined YAML `$schema` for current Markdown file */
-    const schemaPath = data.$schema;
-    /* Path is combined with process current working directory */
-    const schemaFullPath = path.join(process.cwd(), schemaPath);
-    const schema = yaml.parse(
-      // IDEA: make it async?
-      fs.readFileSync(schemaFullPath, 'utf-8'),
-    );
-    /* `$schema` is now extracted, remove it so it will not interfere */
-    delete data.$schema;
-
-    /* JSON Schema compilation + validation with AJV */
-    const validate = ajv.compile(schema);
-    validate(data);
-
-    /* Push JSON Schema validation failures messages */
-    validate?.errors?.forEach((error) => {
-      /* Capitalize error message */
-      const errMessage =
-        `${error.message.charAt(0).toUpperCase()}` +
-        `${error.message.substring(1)}`;
-      /* Parent or sub-property error */
-      const reason = error.instancePath
-        ? `${error.instancePath}: ${errMessage}`
-        : errMessage;
-
-      const message = new VFileMessage(
-        reason,
-        // TODO: find a way to map code range to validation errors
-        // position,
-        null,
-        // NOTE: `origin` doesn't seems to be leveraged by anything
-        // 'origin',
-      );
-
-      /* Assemble and format pretty per-error insights for end-user */
-      message.note = yaml
-        .stringify({
-          keyword: error.keyword,
-          params: error.params,
-          $schema: `${schemaPath}/${error.schemaPath}`,
-        })
-        .trim();
-      // FIXME: beware that this is not working correctly with `auto-fix`,
-      // can be dangerous (wrong code range)!
-      message.expected = error?.params?.allowedValues;
-
-      file.messages.push(message);
-    });
+    /* Handle only if the current Markdown file has a frontmatter section */
+    if (Array.isArray(ast?.children)) {
+      if (ast.children[0]?.value) {
+        validateFrontmatter(ast.children[0]?.value, file);
+      }
+    }
   },
 );
 
