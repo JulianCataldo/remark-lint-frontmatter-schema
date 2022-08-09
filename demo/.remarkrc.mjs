091629511ce8d2b1354dc0352de1df529eb2c868
Julian Cataldo <contact@juliancataldo.com>
2022-08-09 05:23:24 +0200

    feat: update demo to reflect global settings

--- demo/.remarkrc.mjs
+++ demo/.remarkrc.mjs
@@ -1,9 +1,32 @@
-import remarkFrontmatter from "remark-frontmatter";
-import rlFmSchema from "@julian_cataldo/remark-lint-frontmatter-schema";
+import remarkFrontmatter from 'remark-frontmatter';
+import rlFmSchema from '@julian_cataldo/remark-lint-frontmatter-schema';
 /* —————————————————————————————————————————————————————————————————————————— */
 
 const remarkConfig = {
-  plugins: [remarkFrontmatter, rlFmSchema],
+  plugins: [
+    remarkFrontmatter,
+    /*  v———— Use it without settings, with local '$schema' associations only */
+    // rlFmSchema
+
+    /*  v———— Or with global schemas associations */
+    [
+      rlFmSchema,
+      {
+        schemas: {
+          /* One schema for many files */
+          './content/creative-work.schema.yaml': [
+            /* Support glob patterns */
+            './content/*-creative-work.md',
+            // './content/does-not-exist-anymore.md',
+          ],
+          // './content/ghost.schema.yaml': [
+          //   './content/casper.md',
+          //   './content/ether.md',
+          // ],
+        },
+      },
+    ],
+  ],
 };
 
 export default remarkConfig;
