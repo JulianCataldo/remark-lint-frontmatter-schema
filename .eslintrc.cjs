24d097c5210ec4771bec48f47abe3277794e5658
Julian Cataldo <contact@juliancataldo.com>
2022-09-23 17:26:43 +0200

    docs: add dotfiles references

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,13 @@
 /** @type {import("@types/eslint").Linter.Config} */
 
 module.exports = {
+  /**
+   * References:
+   *
+   * https://github.com/JulianCataldo/web-garden/blob/develop/configs/eslint-js.cjs
+   * https://github.com/JulianCataldo/web-garden/blob/develop/configs/eslint-ts.cjs
+   *
+   * */
   extends: [
     './node_modules/@julian_cataldo/astro-configs/eslint-js.cjs',
     './node_modules/@julian_cataldo/astro-configs/eslint-ts.cjs',
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,11 @@
 /** @type {import("@types/prettier").Options} */
 
 module.exports = {
+  /**
+   * Reference:
+   *
+   * https://github.com/JulianCataldo/web-garden/blob/develop/configs/prettier-base.cjs
+   *
+   * */
   ...require('@julian_cataldo/astro-configs/prettier-base.cjs'),
 };
